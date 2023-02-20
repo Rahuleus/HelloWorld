b1ee21602ad0464cfaa5b8502e7632bb8c8a37f7
Rahuleus <124668984+Rahuleus@users.noreply.github.com>
2023-02-20 22:37:20 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ class HelloWorld
     public static void main(String args[]){
         System.out.println("HelloWorld");
         System.out.println("NO i will not");
+        System.out.println("I wonder how i wonder why :D");
     }
 }
