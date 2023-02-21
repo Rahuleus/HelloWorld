25592a6ab1a78392eebf4f2b38292b48e1c4dc60
Rahuleus <124668984+Rahuleus@users.noreply.github.com>
2023-02-21 14:59:14 +0530

    dynamic

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
-public Class helloWorld
+import java.util.*;
+public class helloWorld
 {
-    public static void Main(String args[]){
+    public static void main(String args[]){
         System.out.println("HelloWorld");
         System.out.println("NO i will not");
         System.out.println("I wonder how i wonder why :D");
